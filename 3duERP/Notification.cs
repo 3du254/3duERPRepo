4046aebeee1cdce752771841bdb8a0f8dc6cc1fa
Kemboi Edwin <kemboi.edwin@intsoftkenya.co.ke>
2019-01-25 14:22:04 +0300

    New Changes

---
+++ 3duERP/Notification.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace _3duERP
+{
+    public class Notification
+    {
+        public void RegisterNotification(DateTime CurrentTime)
+        {
+            string conString = ConfigurationManager.ConnectionStrings["conString"].ConnectionString;
+            string SQLCommand = @"SELECT [ContactID],[ContactName],[ContactNo],[AddedOn] FROM [dbo].[Contacts] WHERE [AddedOn]>@AddedOn";
+            using (SqlConnection con= new SqlConnection(conString))
+            {
+                using (SqlCommand cmd=new SqlCommand(SQLCommand, con))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    cmd.Parameters.AddWithValue("@AddedOn",CurrentTime);
+                    if (con.State == ConnectionState.Closed)
+                    {
+                        con.Open();
+                    }
+                    cmd.Notification = null;
+                    SqlDependency SQLDep = new SqlDependency(cmd);
+                    SQLDep.OnChange += SQLDep_OnChange;
+                    using (SqlDataReader SQLReader = cmd.ExecuteReader())
+                    {
+
+                    }
+                }
+
+            }
+        }
+
+        private void SQLDep_OnChange(object sender, SqlNotificationEventArgs e)
+        {
+            if (e.Type == SqlNotificationType.Change)
+            {
+                SqlDependency _SqlDependency = sender as SqlDependency;
+                _SqlDependency.OnChange -= SQLDep_OnChange;
+
+                //send notification to client
+                var _PushNotification = GlobalHost.ConnectionManager.GetHubContext<PushNotification>();
+                _PushNotification.Clients.All.notify("added");
+                //re register notification
+
+            }
+        }
+    }
+}
---
+++ 3duERP/PushNotification.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace _3duERP
+{
+    public class PushNotification : Hub
+    {
+        //public void Hello()
+        //{
+        //    Clients.All.hello();
+        //}
+    }
+}
